19bd3ca01f91c518411df8998144eef05553047a
SangeethaShanmugm <sanjuriya22@gmail.com>
2023-11-05 19:11:16 +0530

    updated

--- JavaScript/10stringOperations.js
+++ JavaScript/10stringOperations.js
@@ -52,3 +52,64 @@ console.log(city.slice(0, -3))//amsTer
 console.log(city.slice(-2, 2))//""
 console.log(city.slice(-3, -1))//da
 
+
+var city = "amsTerdam"
+city.charAt(0).toUpperCase()//A
+
+city.slice(1).toLowerCase()//msterdam
+
+city.charAt(0).toUpperCase() + city.slice(1).toLowerCase()
+"A" + "msterdam" // Amsterdam
+
+city = "paRIs"
+city.charAt(0).toUpperCase() + city.slice(3).toLowerCase()
+"P" + "is" //"Pis"
+
+var uname = "AbdulKalam"
+uname.length//10
+
+var uname = "  AbdulKalam .  "
+console.log(uname.length)//16
+console.log(uname.trim())//AbdulKalam .
+
+var a = "I am learning Javascript";
+a.replace("Javascript", "JS")//I am learning JS
+
+var a = "JavaScript I am learning Javascript";
+a.replace("Javascript", "JS")//JavaScript I am learning JS
+
+var a = "JavaScript I am learning JavaScript";
+a.replace(/JavaScript/g, "JS")//JS I am learning JS
+
+var name = "  AbdulKalam .  "
+console.log(name.replaceAll(" ", ""))//AbdulKalam.
+
+
+//replaceAll(pattern, replacement)
+
+const string = "Hello everyone hi hi"
+let pattern = "hi"
+let replacement = "hey"
+let new_string = string.replaceAll(pattern, replacement)
+console.log(new_string)//Hello everyone hey hey
+
+const a1 = "Hello everyone good good day"
+console.log(a1.replace(/good/g, "?"))//Hello everyone ? ? day
+
+//split
+
+var a = "https://github.com/SangeethaShanmugm/ED-Sep-intern-23"
+console.log(a.split("/"))//['https:', '', 'github.com', 'SangeethaShanmugm', 'ED-Sep-intern-23']
+
+const link = "https://github.com/SangeethaShanmugm?tab=repositories"
+console.log(link.split("?"))//['https://github.com/SangeethaShanmugm', 'tab=repositories']
+
+var a = "JavaScript"
+console.log(a.split(""))//['J', 'a', 'v', 'a', 'S', 'c', 'r', 'i', 'p', 't']
+
+
+var b = ['J', 'a', 'v', 'a', 'S', 'c', 'r', 'i', 'p', 't']
+console.log(b.toString())//J,a,v,a,S,c,r,i,p,t
+
+
+console.log(b.toString().replaceAll(",", ""))//JavaScript
---
+++ JavaScript/11array.js
@@ -0,0 +1,101 @@
+// Array is a collection of homogenious as well as heterogenous data types
+
+//homogenious - same data types
+let a = ["Hii", "Hello", "hii", "dugdugh", `Hey`]//Array of strings
+let b = [1, 4, , 23, 78, 45.67] //Array of numbers
+let c = [true, false, false, true]//Array fo Boolean
+
+//heterogenous  - different data types
+
+let d = ["Hii", 50, true, 43, "hey", "text"]
+
+var e = [3, 5, 7, 9]
+console.log(typeof e) //object
+
+
+var city = ["Delhi", "Mumbai", "Helsinki", "London", "Amsterdam"]
+
+//["Delhi", "Mumbai", "Helsinki", "London", "Amsterdam"]
+//    0       1            2           3        4
+
+city[0]//Delhi
+city[3]//London
+
+//push => add at the end of array
+
+
+var city = ["Delhi", "Mumbai", "Helsinki", "London", "Amsterdam"]
+console.log(city) //['Delhi', 'Mumbai', 'Helsinki', 'London', 'Amsterdam']
+
+console.log(city.push("Boston"))//6
+console.log(city)// ['Delhi', 'Mumbai', 'Helsinki', 'London', 'Amsterdam', 'Boston']
+
+city.push("Venice")
+console.log(city)//['Delhi', 'Mumbai', 'Helsinki', 'London', 'Amsterdam', 'Boston','Venice']
+
+//pop => remove last element in an array 
+
+var city = ['Delhi', 'Mumbai', 'Helsinki', 'London', 'Amsterdam', 'Boston', 'Venice']
+console.log(city.pop())//Venice
+console.log(city)//['Delhi', 'Mumbai', 'Helsinki', 'London', 'Amsterdam', 'Boston']
+
+city.pop()//Boston
+console.log(city)//['Delhi', 'Mumbai', 'Helsinki', 'London', 'Amsterdam']
+
+//unshift => add an element to start of array
+
+var city = ['Delhi', 'Mumbai', 'Helsinki', 'London', 'Amsterdam', 'Boston', 'Venice']
+
+console.log(city.unshift("Dubai"))//8
+console.log(city)//['Dubai','Delhi', 'Mumbai', 'Helsinki', 'London', 'Amsterdam', 'Boston', 'Venice']
+
+
+var train = ["🚃", "🚃", "🚃"]
+
+console.log(train.unshift("🚂", "🔗"))//5
+console.log(train)//["🚂", "🔗","🚃", "🚃", "🚃"]
+
+//shift => remove element from start of an array 
+
+console.log(train.shift())//🚂
+console.log(train)//['🔗', '🚃', '🚃', '🚃']
+
+var farm = ["🐷", "🐐", "🐴", "🐮", "🐔", "🐃"]
+
+console.log(farm.shift())//["🐐", "🐴", "🐮", "🐔", "🐃"]
+console.log(farm.shift())//["🐴", "🐮", "🐔", "🐃"]
+console.log(farm.push("🐟"))
+console.log(farm)//['🐴', '🐮', '🐔', '🐃', '🐟']
+
+
+//slice => slice(startIndex, endIndex)
+
+
+var farm = ["🐷", "🐐", "🐴", "🐮", "🐔", "🐃"]
+//["🐷", "🐐", "🐴", "🐮", "🐔", "🐃"]
+//   0      1     2      3     4     5
+//  -6     -5    -4     -3    -2    -1
+
+console.log(farm.slice(2, 5))//["🐴", "🐮", "🐔",]
+console.log(farm.slice(-4, -1))//["🐴", "🐮", "🐔",]
+
+//splice => splice(startIndex, DeleteCount, Values)
+
+
+var city = ['Delhi', 'Mumbai', 'Helsinki', 'London', 'Amsterdam', 'Boston', 'Venice']
+//             0        1          2
+
+console.log(city.splice(2, 2))//['Helsinki', 'London']
+console.log(city)// ['Delhi', 'Mumbai', 'Amsterdam', 'Boston', 'Venice']
+
+console.log(city.splice(3, 0))//
+console.log(city)//['Delhi', 'Mumbai', 'Amsterdam', 'Boston', 'Venice']
+
+console.log(city.splice(3, 0, "Pune", "Paris"))
+//['Delhi', 'Mumbai', 'Amsterdam',"Pune", "Paris", 'Boston', 'Venice']
+console.log(city)
+
+console.log(city.splice(1, 1, "Innsburg", "France"))//['Mumbai']
+console.log(city)//['Delhi', 'Innsburg', 'France', 'Amsterdam', 'Pune', 'Paris', 'Boston', 'Venice']
+
+
